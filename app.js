34edb02e000eb6326110852b6709de8056dd4293
RAND <nisithadranaweera@gmail.com>
2023-07-31 23:45:14 +0530

    added axios

--- app.js
+++ app.js
@@ -11,14 +11,14 @@ const app = express();
 app.use(express.json());
 app.use(userRouter);
 
-const test = async (email, password)=>{
-    const user = await User.findOne({email:email});
-    const result = await user.comparePassword(password);
-    console.log(result);
-}
-test("ranaweera@gmail.com","RANDRanwera")
+// const test = async (email, password)=>{
+//     const user = await User.findOne({email:email});
+//     const result = await user.comparePassword(password);
+//     console.log(result);
+// }
+// test("ranaweera@gmail.com","RANDRanwera")
 app.get('/', (req,res)=>{
-    res.send('<h1 style ="color :red">HW</h1>');
+    res.json({success:true, message:'Backend connected'});
 });
 
 app.listen(8000,()=> {
