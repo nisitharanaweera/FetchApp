0bb7f8259b114cf31b83156a241dc4e775849c77
RAND <nisithadranaweera@gmail.com>
2023-08-03 05:13:58 +0530

    added cloudinary for profile image storing

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,8 @@
 
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
+const sharp = require('sharp');
+const cloudinary = require('../helper/imageUpload')
 
 exports.createUser =  async(req,res)=>{
     const { name, email, password} = req.body;
@@ -38,4 +40,30 @@ exports.userSignIn = async (req,res)=>{
    const token = jwt.sign({userId:user._id}, process.env.JWT_SECRET,{expiresIn:'1d'});
 
     res.json({success:true, user, token});
+}
+
+
+exports.uploadProfile = async (req,res)=>{
+    const {user}= req
+    if(!user) return res. status(401).json({success: false, message: 'Unauthorised Access'});
+    
+    
+
+    try {
+        const result = await cloudinary.uploader.upload(req.file.path,{
+            public_id: `${user._id}_profile`,
+            width:500,
+            height:500,
+            crop:'fill'
+        });
+        
+        await User.findByIdAndUpdate(user._id,{avatar: result.url});
+        res.status(201).json({success:true,message:'Profile Image Updated'});
+    } catch (error) {
+        res.status(500).json({success:false,message:'Server Error, Profile Image Unchanged'});
+
+        console.log('Error Uploading the Profile Image',error.message);
+        
+    }
+    
 }
--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const User = require('../models/user');
 const router = express.Router();
-const { createUser, userSignIn } = require('../controllers/user');
+const { createUser, userSignIn, uploadProfile } = require('../controllers/user');
 const { validateUserSignUp, userValidation, validateUserSignIn } = require('../middlewares/validation/user');
 const { isAuth } = require('../middlewares/auth');
-const sharp = require('sharp');
+
 const multer = require('multer');
 
 
-const storage = multer.memoryStorage();
+const storage = multer.diskStorage({})
 const fileFilter = (req,file,cb)=>{
     if(file.mimetype.startsWith('image')){
         cb(null,true);
@@ -26,23 +26,5 @@ router.post('/create-user',validateUserSignUp,userValidation, createUser);
 // router.post('/create-post', isAuth,(req,res)=>{
 //     res.send('accessed secret route');
 // })
-router.post('/upload-profile', isAuth, uploads.single('profile'), async (req,res)=>{
-    const {user}= req
-    if(!user) return res. status(401).json({success: false, message: 'Unauthorised Access'});
-    
-    try {
-        const profilebuffer = req.file.buffer;
-        const {width, height} = await sharp(profilebuffer).metadata();
-        const avatar = await sharp(profilebuffer).resize(Math.round(width*0.5),Math.round(height*0.5)).toBuffer()
-    
-        await User.findByIdAndUpdate(user._id,{avatar});
-        res.status(201).json({success:true,message:'Profile Image Updated'});
-    } catch (error) {
-        res.status(500).json({success:false,message:'Server Error, Profile Image Unchanged'});
-
-        console.log('Error Uploading the Profile Image',error.message);
-        
-    }
-    
-})
+router.post('/upload-profile', isAuth, uploads.single('profile'), uploadProfile )
 module.exports = router;
